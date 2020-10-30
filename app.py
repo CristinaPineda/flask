79dfbf821eeeba4a486dbfff7e7676113819298b
CristinaPineda <cristinadaspineda@yahoo.com.br>
2020-10-30 19:54:53 +0000

    final da aula

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', lista=['cris', 'isa', 'edi'])
     
 
 @app.route('/<nome>')
