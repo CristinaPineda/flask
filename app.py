c31666ec952929b2fc13e3f0e4e4723bda4745ca
CristinaPineda <cristinadaspineda@yahoo.com.br>
2020-10-30 19:41:02 +0000

    com rotas variáveis e templates

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "testando minha pagina, TE AMO SEU CHATO"
+    return render_template('index.html')
+    
 
+@app.route('/<nome>')
+def ola_com_nome(nome):
+    return render_template('index.html', nome_pessoa=nome)
